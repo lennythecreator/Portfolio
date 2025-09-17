ef14a51dd56c9f39c6ca801cc49b1e0b39d6d606
lennythecreator <lenyeuwaeme@gmail.com>
2025-09-17 02:03:37 -0400

    updated projects page and added tooltip for projects

--- app/Projects/page.js
+++ app/Projects/page.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader } from '@/components/ui/card'
 import { PROJECTS } from '@/constants/constants'
 import { Github } from 'lucide-react';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 
 export default function page() {
   return (
@@ -10,7 +11,7 @@ export default function page() {
         <Navbar/>
         <div className='px-20 py-10'>
           <h1 className='text-4xl font-bold py-2'>Projects</h1>
-          <p>These projects demostate my ability to think outside of the box and use these ML models in  cool and new intersting ways</p>
+          <p>These projects demostate my ability to think outside of the box and use these AI models in  cool and new intersting ways</p>
 
           <div className='flex flex-col gap-5 p-4'>
               {PROJECTS.map((proj,index)=>(
@@ -18,9 +19,17 @@ export default function page() {
                     <CardHeader className=' flex py-6 font-medium'>
                       <span className='flex'>
                         <p>{proj.name}</p>
-                        <a href={proj.link} target='_blank' rel='noreferrer' className='ml-auto mr-6 p-2 rounded-full bg-blue-100 hover:bg-slate-200 transition-colors ease-in-out'>
-                          <Github className='text-zinc-800 hover:text-blue-600 transition-colors ease-in-out'/>
-                        </a>
+                        <Tooltip>
+                          <TooltipTrigger asChild>
+                            <a href={proj.link} target='_blank' rel='noreferrer' className='ml-auto mr-6 p-2 rounded-full bg-blue-100 hover:bg-slate-200 transition-colors ease-in-out'>
+                              <Github className='text-zinc-800 hover:text-blue-600 transition-colors ease-in-out'/>
+                            </a>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <p>View on GitHub</p>
+                          </TooltipContent>
+                        </Tooltip>
+                        
                       </span>
                     </CardHeader>
                     <CardDescription className='flex gap-2 px-6 pb-6'>{proj.technologies.map((tech,index)=>(
